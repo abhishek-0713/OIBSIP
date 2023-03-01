7a7f6057cecdc1eb2274b3743d8261eaf39be9c9
Coder Abhi <coder.abhi0713@gmail.com>
2023-03-01 21:55:42 +0530

    online-examination-system

--- online-examination-system/src/Login.java
+++ online-examination-system/src/Login.java
@@ -16,11 +16,12 @@ class Login extends JFrame implements ActionListener {
         passLabel = new JLabel("Password: ");
         textField2 = new JPasswordField(8);
         submitButton = new JButton("SUBMIT");
-        newPanel = new JPanel(new GridLayout(3, 1));
+        newPanel = new JPanel(new GridLayout(3, 2));
         newPanel.add(userLabel);
         newPanel.add(textField1);
         newPanel.add(passLabel);
         newPanel.add(textField2);
+        newPanel.add(new JLabel());
         newPanel.add(submitButton);
         add(newPanel, BorderLayout.CENTER);
         submitButton.addActionListener(this);
@@ -30,6 +31,7 @@ class Login extends JFrame implements ActionListener {
         setResizable(false); // prevent resizing the window
         pack(); // adjust the size of the window to fit the components
         setVisible(true);
+
     }
 
     public void actionPerformed(ActionEvent ae) {
